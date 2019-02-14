e7d46e865a2e77070b4a2c32bc2c4a006460661f
Joy Lin <vvlnote@hotmail.com>
2019-02-14 23:41:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def get_user_input
   answer = gets.chomp
 end
 
-def end_game
+def end_game total
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
